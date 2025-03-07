1ae882f99f5560fcae3552533670c945c8a1a29e
Ramon_Fonseca <ramon.taco678@gmail.com>
2025-03-07 08:24:55 -0600

    miprimerproyecto2.1

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from src import taller_mecanico
-
 taller_mecanico.mi_funcion()
+def leer_readme():
+    with open("README.md", "r", encoding="utf-8") as archivo:
+        contenido = archivo.read()
+        return contenido
+
+if __name__ == "__main__":
+    contenido_readme = leer_readme()
+    print(contenido_readme)
